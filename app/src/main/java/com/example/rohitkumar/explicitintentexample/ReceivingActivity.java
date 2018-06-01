e917caaa1fd656854368b6eec8c8e9a1f7291d2e
Rohit Kumar <rohit9075@gmail.com>
2018-06-01 08:37:12 +0530

    comments

--- app/src/main/java/com/example/rohitkumar/explicitintentexample/ReceivingActivity.java
+++ app/src/main/java/com/example/rohitkumar/explicitintentexample/ReceivingActivity.java
@@ -14,26 +14,33 @@ public class ReceivingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receiving);
 
-
+       //  gettingReference() method call
         gettingReference();
 
+        //  getting the name and email from textInputLayout
         String  mName = getIntent().getStringExtra("name");
         String  mEmail = getIntent().getStringExtra("email");
 
 
+        // setting the name and email to the textView
         mTextViewName.setText(mName);
         mTextViewEmail.setText(mEmail);
 
+        // Bundle object
         Bundle bundle = getIntent().getExtras();
+
+        // receiving the name and email from bundle using key and storing in local varialble
         String bundleName = bundle.getString("bundle_name");
         String bundleEamil = bundle.getString("bundle_email");
 
+        // setting the name and email to the textView
         textViewBundleName.setText(bundleName);
         textViewBundleEamil.setText(bundleEamil);
 
 
     }
 
+    //  gettingReference() method definition
     private void gettingReference() {
 
         mTextViewName = findViewById(R.id.intentName);
